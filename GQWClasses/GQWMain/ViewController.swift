38712e6a999a657d0134d6479367c54de2b96ab6
feibahuolong1 <liweipeng@liweipengdeMac-mini.local>
2017-06-05 15:55:35 +0800

    构建项目结构

---
+++ GQWClasses/GQWMain/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  TestGQWWithSwift
+//
+//  Created by Broc on 2017/6/5.
+//  Copyright © 2017年 Broc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.blue
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
